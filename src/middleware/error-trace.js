072185464214f7e70b692c6aaaedcd4959b04168
diggzhang <diggzhang@gmail.com>
2016-11-08 16:06:11 +0800

    Added chalk to pretty print error log

--- src/middleware/error-trace.js
+++ src/middleware/error-trace.js
@@ -1,13 +1,17 @@
 "use strict";
 
+const chalk = require("chalk");
+
 module.exports = function errorTrace() {
     return function *(next) {
         try {
             yield next;
         }
         catch (err) {
+            let errorTime = Date.now()
+            console.error(chalk.red("["));
             // debug detail
-            console.error(Date.now());
+            console.error(chalk.red(errorTime));
             console.error(this.url);
             console.error(this.request.body);
 
@@ -23,6 +27,8 @@ module.exports = function errorTrace() {
             this.body = {msg: err.message};
             this.status = err.status || 500;
             this.app.emit('error', err, this);
+            console.error(chalk.red(errorTime));
+            console.error(chalk.red("]"));
 
             // return http status 204 is to ensure that the client retransmission event
             this.status = 204;
