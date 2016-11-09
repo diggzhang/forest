017cc791fc4a73e2288dcef8a5b2dd49c6bb6d92
diggzhang <diggzhang@gmail.com>
2016-11-09 17:11:40 +0800

    Add api doc

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,14 +4,27 @@
 const router = require("koa-router")({"prefix": "/api"});
 
 
-/**
- * router mount
- */
-router.use("/v4/events", require("../routes/eventv4").routes());
+router.use("/v4", require("../routes/v4Api").routes());
 
 
 /**
- *  Survival API ping-pong test
+ * @api {GET} /api/ping Survial ping-pong test
+ * @apiName PingPingTest
+ * @apiGroup ForAll
+ *
+ * @apiParam {None} None 无参
+ *
+ * @apiSuccess {String} body 成功后body体返回 pong <unix timestamp>
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     pong 1478679370262
+ *
+ * @apiError NotFound request method not found or server crash.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     Body: Not Found
  */
 router.all("/ping", function *() {
     this.body = "pong " + Date.now();
